cfa711525e38a8faca94d98948d04c2c59a76e77
Jon Q <hello@jonquach.com>
2018-12-14 15:45:12 -0500

    Remove babel-react-app

--- babel.js
+++ babel.js
@@ -2,7 +2,6 @@ const kcdBabel = require('./dist/config/babelrc')()
 
 module.exports = function zeroBabel() {
   return {
-    presets: kcdBabel.presets.concat(require.resolve('babel-preset-react-app')),
     plugins: kcdBabel.plugins.concat(
       require.resolve('@babel/plugin-transform-flow-strip-types'),
       require.resolve('babel-plugin-inline-svg'),
