4064d7ea72480dbef9a2d8431cebdee3092ee5f4
Jon Q <hello@jonquach.com>
2018-12-14 16:05:30 -0500

    Adjust babelrc

--- babel.js
+++ babel.js
@@ -2,6 +2,7 @@ const kcdBabel = require('./dist/config/babelrc')()
 
 module.exports = function zeroBabel() {
   return {
+    presets: kcdBabel.presets,
     plugins: kcdBabel.plugins.concat(
       require.resolve('@babel/plugin-transform-flow-strip-types'),
       require.resolve('babel-plugin-inline-svg'),
