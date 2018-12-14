e34d9d45cabf72a78b669046fc2b863247ee3600
Jon Q <hello@jonquach.com>
2018-12-14 10:03:51 -0500

    Adjust ESLint

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@
 
 const args = process.argv.slice(2)
 const command = args[0]
+const cmd = command && command.toLowerCase()
 
-if (command && command.toLowerCase() === 'build') {
+if (cmd && cmd === 'build') {
   /* eslint dot-notation: 0 */
   if (!process.env['BABEL_ENV']) {
     process.env['BABEL_ENV'] = 'production'
   }
 }
 
-if (command && command.toLowerCase() === 'prestart') {
+if (cmd && cmd === 'prestart') {
   const prestart = require('@helpscout/prestart')
   prestart.sync()
-} else {
-  require('kcd-scripts/dist/index')
+  return
 }
+
+require('kcd-scripts/dist/index')
